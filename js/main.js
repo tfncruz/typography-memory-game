dd51324a55176de8c808859611501c913a9db09f
Tiago Cruz <tfncruz@gmail.com>
2018-03-17 20:27:48 +0000

    make first functional version

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
 const main = document.querySelector("main");
 
-let cards = new Array();
+const cards = new Array();
 
+let cardSelected1;
+let cardSelected2;
+let noMatch = false;
+
+let cardsFound = 0;
+
+/*
+* Load cards array with cards (css class)
+*/
 function loadCards() {
-	let cardCount = 1;
+
+	let imageCount = 1;
 
 	for(let i = 0; i < 16; i += 2) {
-		cards[i] = "background-image: url('./imgs/img"+cardCount+".svg');";
-		cards[i+1] = "background-image: url('./imgs/img"+cardCount+".svg');";
-		cardCount++;
+		cards[i] = "image"+imageCount;
+		cards[i+1] = "image"+imageCount;
+
+		imageCount++;
 	}
 
-	shuffleCards();
+	shuffleImages();
 }
 
-function shuffleCards() {
+/*
+* shuffle the cards array
+*/
+function shuffleImages() {
+
 	let currentIndex = cards.length-1;
 
 	while(currentIndex >= 0) {
@@ -26,13 +41,64 @@ function shuffleCards() {
 	}
 }
 
+function clearLastTwo() {
+	let classes1 = cardSelected1.classList;
+	let classes2 = cardSelected2.classList;
+
+	cardSelected1.classList.toggle(classes1[1]);
+	cardSelected2.classList.toggle(classes2[1]);
+	cardSelected1.classList.toggle("back");
+	cardSelected2.classList.toggle("back");
+
+	cardSelected1 = undefined;
+	cardSelected2 = undefined;
+
+	noMatch = false;
+}
+
+// load the cards array with the images
 loadCards();
 
+// -------------------------------------------------- Event listeners and handlers
 main.addEventListener("click", function(event) {
 
-	const elem = event.target;
-	const row = elem.id.split("_");
-	elem.style.cssText = cards[row[1]-1];
+	const element = event.target;
+
+	// if the click was in a available card and cardsFound less than 8
+	if(element.classList.contains("card-container") && element.classList.contains("back") && cardsFound < 8) {
+
+		if(noMatch) { clearLastTwo(); }
+
+		// turn the card and store a reference to that element in ref1 or ref2
+		const row = event.target.id.split("_");
+		element.classList.toggle(cards[row[1]-1]);
+		element.classList.toggle("back");
+
+		if(cardSelected1 === undefined) { cardSelected1 = element; }
+		else { cardSelected2 = element; }
+
+		// if ref1 and ref2
+		if(cardSelected1 !== undefined && cardSelected2 !== undefined) {
+
+			// compare
+			classes1 = cardSelected1.classList;
+			classes2 = cardSelected2.classList;
+			if(classes1[1] !== classes2[1]) {
+				// set this to true so that in the next click these two cards are flipped back
+				noMatch = true;
+			} else {
+				++cardsFound;
+				//reset cards selected
+				cardSelected1 = undefined;
+				cardSelected2 = undefined;
+			}
+		}
+
+		// if cardsFound equals 8, won
+		if(cardsFound === 8) {
+			console.log("YOU WON!!!");
+		}
+	}
 });
 
 
